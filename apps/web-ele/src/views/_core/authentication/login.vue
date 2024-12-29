db7bd9a805f8b0a44ba17556823b8afba754bd8b
YangXu <2945065490@qq.com>
2024-12-29 17:46:02 +0800

    feat: 初始化基本完成，开始编写

--- apps/web-ele/src/views/_core/authentication/login.vue
+++ apps/web-ele/src/views/_core/authentication/login.vue
@@ -93,6 +93,8 @@ const formSchema = computed((): VbenFormSchema[] => {
   <AuthenticationLogin
     :form-schema="formSchema"
     :loading="authStore.loginLoading"
+    :show-qrcode-login="false"
+    :show-third-party-login="false"
     @submit="authStore.authLogin"
   />
 </template>
